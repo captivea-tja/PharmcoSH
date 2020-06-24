ae3610af6908f966f8e2a574c62fd5356b6a7374
Timothy James <52675656+captivea-tja@users.noreply.github.com>
2020-06-24 10:50:33 -0400

    Add files via upload

---
+++ cap_mrp_customization/models/stock_move_line.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    manufacturer_lot = fields.Char(string="Manufacturer's Lot")
+    expiration_date = fields.Date(string="Expiration Date")
+    tare_weight = fields.Float(string="Tare Weight")
+    gross_weight = fields.Float(string="Gross Weight")
+    container_type = fields.Char(string="Container Type")
+    manufacture_date = fields.Date(string="Date of Manufacture")
+
+    @api.constrains('manufacture_date', 'expiration_date')
+    def _check_date(self):
+        for line in self:
+            if line.manufacture_date and line.expiration_date and \
+                    line.expiration_date < line.manufacture_date:
+                raise ValidationError(
+                    _('The expiration date cannot be earlier than the manufacture date.'))
+
+    def action_generate_serial(self):
+        self.ensure_one()
+        product_produce_wiz = self.env.ref('stock.view_move_line_form', False)
+        self.lot_id = self.env['stock.production.lot'].create({
+            'product_id': self.product_id.id,
+            'company_id': self.company_id.id
+        })
+
---
+++ cap_mrp_customization/models/stock_picking.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        for move_line in self.move_line_nosuggest_ids:
+            if move_line.lot_id:
+                move_line.lot_id.removal_date = move_line.expiration_date
+                move_line.lot_id.manufacturer_lot = move_line.manufacturer_lot
+                move_line.lot_id.tare_weight = move_line.tare_weight
+                move_line.lot_id.gross_weight = move_line.gross_weight
+                move_line.lot_id.container_type = move_line.container_type
+                move_line.lot_id.manufacture_date = move_line.manufacture_date
+        return super(Picking, self).button_validate()
